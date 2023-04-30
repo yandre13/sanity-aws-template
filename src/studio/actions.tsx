d0131823d76bc985dab8fcdb04f4a1369275bc2f
yandre13 <yandre13.elsf@gmail.com>
2023-04-30 18:24:22 -0500

    add: revalidate

--- src/studio/actions.tsx
+++ src/studio/actions.tsx
@@ -1,34 +1,34 @@
-import { DocumentActionProps } from 'sanity'
+import type { DocumentActionProps, SanityDocument } from 'sanity'
 
-// type RevalidateProps = {
-//   type: string
-//   document: SanityDocument | null
-// }
-// async function callRevalidate({ type, document }: RevalidateProps) {
-//   try {
-//     const res = await fetch(
-//       `/api/revalidate?secret=${process.env.NEXT_PUBLIC_REVALIDATE_SECRET}`,
-//       {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//           data: {
-//             type,
-//             // @ts-ignore
-//             slug: document?.slug?.current,
-//           },
-//         }),
-//       },
-//     )
-//     const data = await res.json()
-//     return data
-//   } catch (err) {
-//     console.log('err', err)
-//     return err
-//   }
-// }
+type RevalidateProps = {
+  type: string
+  document: SanityDocument | null
+}
+async function callRevalidate({ type, document }: RevalidateProps) {
+  try {
+    const res = await fetch(
+      `/api/revalidate?secret=${process.env.NEXT_PUBLIC_REVALIDATE_SECRET}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: {
+            type,
+            // @ts-ignore
+            slug: document?.slug?.current,
+          },
+        }),
+      },
+    )
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log('err', err)
+    return err
+  }
+}
 // async function revalidate({ type, document }: RevalidateProps) {
 //   const res = await callRevalidate({
 //     type,
@@ -48,8 +48,9 @@ export function RevalidateDocumentAction({
     label: 'Revalidate',
     onHandle: async () => {
       // Here you can perform your actions
-      // const res = await callRevalidate({ type, document: published })
+      const res = await callRevalidate({ type, document: published })
       console.log('Page revalidated! ✅')
+      console.log({ res })
       // window.alert('Page revalidated! ✅')
     },
   }
